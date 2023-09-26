f55fca6811921b22a37bd872415a255038f9ac27
Yu Sun <usunyu@gmail.com>
2023-09-25 21:25:41 -0500

    Initial react app

---
+++ client/src/firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDjuKzfM-WSq6HHxEqFHn6O8LC2b36q1Ow',
+  authDomain: 'zillow-tracker-project.firebaseapp.com',
+  projectId: 'zillow-tracker-project',
+  storageBucket: 'zillow-tracker-project.appspot.com',
+  messagingSenderId: '782345412218',
+  appId: '1:782345412218:web:f555bd87443f18c336a5c3',
+  measurementId: 'G-BH47SBWL30',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
